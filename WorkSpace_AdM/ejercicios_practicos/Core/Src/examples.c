5f3be8b4958aff3d3bdf293417f64a12715124e5
“Alex <asantosr@uni.pe>
2022-04-17 00:22:03 -0500

    Solución de ejercicios del 5 al 9 en lenguaje C.

--- WorkSpace_AdM/ejercicios_practicos/Core/Src/examples.c
+++ WorkSpace_AdM/ejercicios_practicos/Core/Src/examples.c
@@ -53,4 +53,88 @@ void productoEscalar12(uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longi
 	}
 }
 
+/**
+ *
+ */
+void filtroVentana10(uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitudVectorIn)
+{
+	if(longitudVectorIn >= 10)
+	{
+		uint8_t longitudVentana = 10;
+		uint16_t suma = 0;
+		uint32_t indexVect;
+		uint32_t indexVent;
+
+		for (indexVect = 0; indexVect < longitudVectorIn; indexVect++)
+		{
+			indexVent = indexVect;
+			for(uint8_t i = 0; i < longitudVentana; i++)
+			{
+				indexVent = (indexVent >= longitudVectorIn) ? (indexVent - longitudVectorIn) : (indexVent);
+				suma += *(vectorIn + indexVent);
+				indexVent++;
+			}
+			*(vectorOut + indexVect) = suma / longitudVentana;
+			suma = 0;
+		}
+	}
+}
+
+/**
+ *
+ */
+void pack32to16 (int32_t * vectorIn, int16_t *vectorOut, uint32_t longitud)
+{
+	for(uint8_t i = 0; i < longitud; i++)
+	{
+		*(vectorOut + i) = (int16_t)(*(vectorIn + i) >> 16);
+	}
+}
+
+/**
+ *
+ */
+int32_t max (int32_t * vectorIn, uint32_t longitud)
+{
+	int32_t max = INT32_MIN;
+	int32_t indexMax = 0;
+	for(uint32_t i = 0; i < longitud; i++)
+	{
+		if(*vectorIn > max)
+		{
+			max = *vectorIn;
+			indexMax = i;
+		}
+		vectorIn++;
+	}
+	return indexMax;
+}
+
+/**
+ *
+ */
+void downsampleM (int32_t * vectorIn, int32_t * vectorOut, uint32_t longitud, uint32_t N)
+{
+	for( uint32_t i = 0; i < longitud; i++)
+	{
+		if (((i + 1) % N) != 0)
+		{
+			*vectorOut++ = *(vectorIn + i);
+		}
+	}
+}
+
+/**
+ *
+ */
+void invertir (uint16_t * vector, uint32_t longitud)
+{
+	uint16_t aux;
+	for(uint32_t i = 0; i < (longitud / 2); i++)
+	{
+		aux = *(vector + i);
+		*(vector + i) = *(vector + longitud - 1 - i);
+		*(vector + longitud - 1 - i) = aux;
+	}
+}
 
